f341f09020738603608c3d3e73f80d0a924a874d
georgepaterson <george.paterson@lbi.com>
2011-08-07 17:40:08 +0100

    Select: setting up UI select file.

---
+++ ui/jquery.ui.select.js
@@ -0,0 +1,23 @@
+/*
+* jQuery UI Select @VERSION
+*
+* Copyright 2011, George Paterson
+* Dual licensed under the MIT or GPL Version 2 licenses.
+* http://jquery.org/license
+*
+* Depends:
+* jquery.ui.core.js
+* jquery.ui.widget.js
+*/
+
+(function($, undefined) {
+	$.widget('ui.select', {
+		version: '@VERSION',
+		options: {
+
+		},
+		_create: function() {
+		
+		}
+	}
+})(jQuery);
