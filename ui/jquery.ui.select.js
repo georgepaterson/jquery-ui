8744cd3dba256391ad63c2ee08087a10913610cb
georgepaterson <george.paterson@lbi.com>
2011-08-08 08:28:20 +0100

    Select: default files for CSS and demo.

--- ui/jquery.ui.select.js
+++ ui/jquery.ui.select.js
@@ -1,14 +1,14 @@
 /*
-* jQuery UI Select @VERSION
-*
-* Copyright 2011, George Paterson
-* Dual licensed under the MIT or GPL Version 2 licenses.
-* http://jquery.org/license
-*
-* Depends:
-* jquery.ui.core.js
-* jquery.ui.widget.js
-*/
+ * jQuery UI Select @VERSION
+ *
+ * Copyright 2011, George Paterson
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Depends:
+ * jquery.ui.core.js
+ * jquery.ui.widget.js
+ */
 
 (function($, undefined) {
 	$.widget('ui.select', {
