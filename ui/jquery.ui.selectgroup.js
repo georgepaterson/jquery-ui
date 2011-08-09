b565a902f1f00421ba2b23670244f3ad0a264ea7
georgepaterson <george.paterson@lbi.com>
2011-08-09 08:56:40 +0100

    Selectgroup: Changing all assets to selectgroup to avoid duplication with select.

---
+++ ui/jquery.ui.selectgroup.js
@@ -0,0 +1,29 @@
+/*
+ * jQuery UI Selectgroup @VERSION
+ *
+ * Copyright 2011, George Paterson
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Depends:
+ * jquery.ui.core.js
+ * jquery.ui.widget.js
+ */
+
+(function($, undefined) {
+	$.widget('ui.selectgroup', {
+		version: '@VERSION',
+		options: {
+
+		},
+		_init: function() {
+			
+		},
+		_create: function() {
+		
+		},
+		destroy: function() {
+
+		}
+	}
+})(jQuery);
