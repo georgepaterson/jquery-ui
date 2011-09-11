5f9b39cedea0689a5c03d6252ee2e5a9210f9a8f
George Paterson <george.paterson@lbi.com>
2011-09-11 23:14:58 +0100

    Selectgroup: Structuring initial working elements.

--- ui/jquery.ui.selectgroup.js
+++ ui/jquery.ui.selectgroup.js
@@ -14,12 +14,13 @@
 	$.widget('ui.selectgroup', {
 		version: '@VERSION',
 		options: {
-
+			placeholder: false
 		},
+		isOpen: false,
 		_create: function() {
 			var self = this, 
 				options = this.options;
-			this.menu = $('<div class="ui-selectgroup-menu"></div>');
+			this.menu = $('<div class="ui-selectgroup-group"></div>');
 			$('body').append(this.menu);
 			this.placeholder = $('<a href="#" class="ui-selectgroup ui-widget ui-state-default ui-corner-all"'
 				+ 'role="button" aria-haspopup="true" aria-owns="">'
@@ -28,15 +29,48 @@
 			$(this.element).after(this.placeholder);
 			$(this.element).hide();
 			this.placeholder.bind('click', function(event) {
-				event.stopPropagation();
 				event.preventDefault();
-				
-			})
+				if (!self.isOpen) {
+					self.open();
+				}
+				else {
+					self.close();
+				}
+			});
 		},
 		_init: function() {
+
+		},
+		_index: function() {
+			this.selectors = $.map($('option', this.element), function(value) {
+				return {
+					text: $(value).text(),
+					optgroup: $(value).parent('optgroup')
+				};
+			});
+			this._build();
+		},
+		_build: function() {
 			var self = this, 
 				options = this.options;
-
+			this.group = $('<ul class="ui-selectgroup-list"></ul>');
+			$.each(this.selectors, function() {
+				var list = '<li class="ui-selectgroup-item"><a href="#">'+ this.text +'</a></li>'
+				if (this.optgroup.length) {
+					var name = 'ui-selectgroup-optgroup-' + self.element.find('optgroup').index(this.optgroup);
+					if (self.group.find('li.' + name).length ) {
+						self.group.find('li.' + name + ' ul').append($(list));
+					}
+					else {
+						var opt = '<li class="' + name + '"><span>'+ this.optgroup.attr('label') +'</span><ul>'+ list +'</ul></li>'
+						$(opt).appendTo(self.group)
+					}
+				}
+				else {
+					$(list).appendTo(self.group);
+				}
+			});
+			$(this.menu).html(this.group);
 		},
 		destroy: function() {
 
@@ -46,6 +80,17 @@
 		},
 		disable: function() {
 
+		},
+		open: function() {
+
+			this._index();
+			this.menu.show();
+			this.isOpen = true;
+		},
+		close: function() {
+			
+			this.menu.hide();
+			this.isOpen = false;
 		}
 	})
 })(jQuery);
